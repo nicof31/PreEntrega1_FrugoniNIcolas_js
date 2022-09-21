d69dcb7f8092e97efb01e807efb86e2706ece396
nicof_31 <nfrugoni31@gmail.com>
2022-09-21 16:47:13 -0300

    programa para subir a repositorio

---
+++ js/generadornumquini.js
@@ -0,0 +1,34 @@
+
+//GENERAR N° QUINI
+
+let iniciar = confirm("Bienvenido al generador N° para jugada Quini 6, para iniciar haga click en ACEPTAR");
+
+if (iniciar == true){
+
+    const VAL_C1 = 5;
+    const VAL_C2 = 60;
+    
+
+//let n1 = Math.ceil(Math.random() * (VAL_C2 - VAL_C1) + VAL_C1 );
+
+
+for (let i=0; i < 6; i+=1) {
+
+
+    let n1 = Math.ceil(Math.random() * 100);
+    //console.log("N° Generado automaticamente : " + n1.toFixed(0));
+        if (n1 > 93) {
+            n1 = (n1 - 10) / 2;
+            console.log("N° Procesado para jugada : " + n1.toFixed(0));
+            document.write('<div class="card_juego_2"><h5>"N° Procesado para jugada : ' + n1.toFixed(0) + ' </h5></div>');
+           
+        } else if (n1 > 46) {
+                n1 = n1 / 2;
+            console.log("Nb° Procesado para jugada: " + n1.toFixed(0));
+            document.write('<div class="card_juego_2"><h5>"N° Procesado para jugada : ' + n1.toFixed(0) + ' </h5></div>');
+        } else {
+            console.log("Nc° Procesado para jugada: " + n1.toFixed(0));
+            document.write('<div class="card_juego_2"><h5>"N° Procesado para jugada : ' + n1.toFixed(0) + ' </h5></div>');
+        }  
+}
+}
